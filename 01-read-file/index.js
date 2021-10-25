ff72d54d8ebf0e02c7bf4595809443cf6d6ba014
Aleksandr Perepelitsa <perepelitsa.alex@gmail.com>
2021-10-25 11:38:44 +0300

    init: start HTML-builder

---
+++ 01-read-file/index.js
@@ -0,0 +1,16 @@
+console.log('start read file');
+
+const path = require('path');
+const fs = require('fs');
+
+let stream = fs.createReadStream(path.join(__dirname + "/text.txt"))
+
+setTimeout(
+  () =>
+    stream.on('data', (data) =>
+      console.log(data.toString())
+    ),
+  3000
+)
+
+
